e80216b130cce7521f12233b0ccdfa059bd36038
sshaw <skye.shaw@gmail.com>
2014-01-20 23:08:19 -0500

    try to normalize the ERN version specs

---
+++ spec/ern/versions_spec.rb
@@ -0,0 +1,23 @@
+require "spec_helper"
+
+shared_examples_for "metadata serialization" do |path|
+  it "serializes the metadata" do
+    xml = File.read(path)
+    # TODO: DDEX.read(xml), DDEX.write
+    spec = DDEX::ERN.read(xml)
+    expect(DDEX::ERN.write(spec)).to equal_xml(xml)
+  end
+end
+
+describe DDEX::ERN do
+  # Again, maybe use a class method to retrieve this info
+  %w[3.4 3.4.1 3.6].each do |spec|
+    describe "ERN #{spec} metadata serialization" do
+      each_fixture("ern/#{ spec.tr(".", "") }").each do |path|
+        context "given #{File.basename(path)}" do
+          include_examples "metadata serialization", path
+        end
+      end
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,12 +3,19 @@ require "test_xml/spec"
 require "ddex"
 
 module SpecHelper
+  FIXTURES = File.join(File.dirname(__FILE__), "fixtures")
+
   def fixture(basename)
     basename << ".xml" unless basename.end_with?(".xml")
-    File.join(File.dirname(__FILE__), "fixtures", basename)
-  end  
+    File.join(FIXTURES, basename)
+  end
+
+  def each_fixture(name)
+    Dir[File.join(FIXTURES, name, "*.xml")]
+  end
 end
 
 RSpec.configure do |cfg|
+  cfg.extend  SpecHelper
   cfg.include SpecHelper
 end
