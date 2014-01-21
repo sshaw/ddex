65fad712ad5291fd208a5882646b59cf9a40a3de
sshaw <skye.shaw@gmail.com>
2014-01-20 23:11:56 -0500

    remove redundant specs

--- spec/ern/34_spec.rb
+++
@@ -1,9 +0,0 @@
-require "spec_helper"
-
-describe DDEX::ERN do
-  it "serializes ERN 3.4" do
-    xml = File.read(fixture("ern/34/instance1"))
-    ern = DDEX::ERN.read(xml)
-    expect(DDEX::ERN.write(ern)).to equal_xml(xml)
-  end
-end
--- spec/ern/36_spec.rb
+++
@@ -1,9 +0,0 @@
-require "spec_helper"
-
-describe DDEX::ERN do
-  it "serializes ERN 3.6" do
-    xml = File.read(fixture("ern/36/instance1"))
-    ern = DDEX::ERN.read(xml)
-    expect(DDEX::ERN.write(ern)).to equal_xml(xml)
-  end
-end
