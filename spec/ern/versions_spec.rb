0d8181c9d91a653886e384561a8258356729a0ad
sshaw <skye.shaw@gmail.com>
2014-02-16 20:00:37 -0500

    add ERN 3.5 [ci skip]

--- spec/ern/versions_spec.rb
+++ spec/ern/versions_spec.rb
@@ -4,14 +4,13 @@ shared_examples_for "metadata serialization" do |path|
   it "serializes the metadata" do
     xml = File.read(path)
     # TODO: DDEX.read(xml), DDEX.write
-    spec = DDEX::ERN.read(xml)
     expect(DDEX::ERN.write(spec)).to equal_xml(xml)
   end
 end
 
 describe DDEX::ERN do
   # Again, maybe use a class method to retrieve this info
-  %w[3.4 3.4.1 3.6].each do |spec|
+  %w[3.4  3.4.1  3.5  3.6].each do |spec|
     describe "ERN #{spec} metadata serialization" do
       each_fixture("ern/#{ spec.tr(".", "") }").each do |path|
         context "given #{File.basename(path)}" do
