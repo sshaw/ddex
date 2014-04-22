2c87af35dea74a1832e6a9c6f9e8c3f680147948
sshaw <skye.shaw@gmail.com>
2014-04-22 02:41:11 -0400

    add some RDocs to the read/write methods

--- lib/ddex.rb
+++ lib/ddex.rb
@@ -25,10 +25,50 @@ module DDEX
     SPECS.any? { |spec| const_get(spec).supports?(name) }
   end
 
-  # TODO: figure out what top level spec...
+  ##
+  # Read DDEX metadata
+  #
+  # === Parameters
+  #
+  # [xml     (String|IO|path)] The XML to read
+  # [options (Hash)] Options to control reading
+  #
+  # === Options
+  #
+  # [:encoding] Parse +xml+ using the given encoding
+  # [:version]  Parse +xml+ as the given version, this must be a +MessageSchemaVersionId+ in +SPEC/VERSION+ format;
+  #		by default this will be taken from the +MessageSchemaVersionId+ attribute of the root element
+  # === Returns
+  #
+  # [DDEX::Element subclass] A root element, as defined by the DDEX spec of +xml+ or the +:version+ option
+  #
+  # === Errors
+  #
+  # [DDEX::XMLLoadError] Problem parsing the XML or a required element is missing
+  # [DDEX::UnknownVersionError] The given or detected spec version is not known or supported
+  # [ArgumentError] +options+ is not a +Hash+
+
   def self.read(xml, options = nil)
+    # TODO: figure out what top level spec...
     DDEX::ERN.read(xml, options)
   end
+
+  ##
+  # Turn a DDEX object into XML
+  #
+  # === Parameters
+  #
+  # [xml     (DDEX::Element subclass)] The object to create XML from
+  # [options (Hash)] Options to control writing
+  #
+  # === Options
+  #
+  # [:schema] Schema URI to use in the schemaLocation attribute, defaults to <code>DDEX::NAME.config["VER"][:schema]</code>
+  #           where +NAME+ is the spec name and +VER+ is the version
+  #
+  # === Errors
+  #
+  # [ArgumentError] +options+ is not a +Hash+ or +xml+ is not a subclass of <code>DDEX::Element</code>
 
   def self.write(xml, options = nil)
     DDEX::ERN.write(xml, options)
